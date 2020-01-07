f8024b8eb70f3c3b7acffe7b50d0ef597a2c6f55
GuerreroBryan <bryan.guerrero01@epn.edu.ec>
2020-01-07 12:06:33 -0500

    splashscreen y edad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,34 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { timer, Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  showSplash = true;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+    private statusBar: StatusBar  
+  )
+  
+  {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.splashScreen.hide();  // <-- hide static image
+
+      timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
 }
